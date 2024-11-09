1d26fadacce9c6b124840a04bd6a05148cbc4598
vitorfolquini <vitorfolquini@hotmail.com>
2024-11-09 18:41:29 -0300

    splashscreen up
    splash screen funcionando

---
+++ app/src/main/java/com/example/weatherappapo/inicializacao.java
@@ -0,0 +1,34 @@
+package com.example.weatherappapo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class inicializacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inicializacao);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // O que fazer após o atraso de 3 segundos: iniciar uma nova Activity
+                Intent intent = new Intent(inicializacao.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Finaliza a SplashScreen para que ela não volte ao pressionar o botão \"voltar\"
+            }
+        }, 3000);
+        }
+    }
